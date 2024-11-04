7fc0a9fa6db8a627812fd8a788a6891fdb93ba79
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-11-04 16:46:29 +0900

    update

--- scripts/count_problem_num.bash
+++ scripts/count_problem_num.bash
@@ -1,3 +1,3 @@
-echo -n $(find ../problem_jp/ | grep GENERAL | wc -l) > cnt_general_num.txt
-echo -n $(find ../problem_jp/ | grep EXERCISE | wc -l) > cnt_execise_num.txt
-echo -n $(find ../problem_jp/ | grep IMAGE | wc -l) > cnt_image_num.txt
+echo $(find ../problem_jp/ | grep GENERAL | wc -l) > cnt_general_num.txt
+echo $(find ../problem_jp/ | grep EXERCISE | wc -l) > cnt_execise_num.txt
+echo $(find ../problem_jp/ | grep IMAGE | wc -l) > cnt_image_num.txt
